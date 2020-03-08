644f0ade6311f671f9cf27a2648476ac6dcb60fb
vmokeev <mokeev.vladimir@gmail.com>
2020-03-09 02:18:15 +0300

    Added matrix exponentional, recurrent sequences

---
+++ src/basic/matrix/MatrixTools.java
@@ -0,0 +1,34 @@
+package basic.matrix;
+
+public class MatrixTools {
+    public static long[][] matrixPower(long[][] base, long pow) {
+        int n = base.length;
+        long[][] res = new long[n][n];
+        for (int i = 0; i < n; i++) {
+            res[i][i] = 1;
+        }
+        while (pow != 0) {
+            if ((pow & 1) != 0) {
+                res = multiplyMatrix(res, base);
+                --pow;
+            } else {
+                base = multiplyMatrix(base, base);
+                pow >>= 1;
+            }
+        }
+        return res;
+    }
+
+    public static long[][] multiplyMatrix(long[][] a, long[][] b) {
+        int n = a.length;
+        long[][] ans = new long[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    ans[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return ans;
+    }
+}
