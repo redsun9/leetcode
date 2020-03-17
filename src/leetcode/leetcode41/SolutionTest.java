1cb8f48a91c59f613368b1f61f12008d17a28b8d
redsun9 <mokeev.vladimir@gmail.com>
2020-03-17 16:00:12 +0300

    Added leetcode41

---
+++ src/leetcode/leetcode41/SolutionTest.java
@@ -0,0 +1,16 @@
+package leetcode.leetcode41;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+    @Test
+    void firstMissingPositive() {
+        Solution solution = new Solution();
+        assertEquals(3, solution.firstMissingPositive(new int[]{1, 2, 0}));
+        assertEquals(2, solution.firstMissingPositive(new int[]{3, 4, -1, 1}));
+        assertEquals(1, solution.firstMissingPositive(new int[]{7, 8, 9, 11, 12}));
+        assertEquals(5, solution.firstMissingPositive(new int[]{2, 1, 4, 3}));
+    }
+}
