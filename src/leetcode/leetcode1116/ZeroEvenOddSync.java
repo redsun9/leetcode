cd8bcf8c254ebf03c5bf8c45a5ed209507f9a348
redsun9 <mokeev.vladimir@gmail.com>
2020-03-19 17:09:53 +0300

    Added leetcode1116

---
+++ src/leetcode/leetcode1116/ZeroEvenOddSync.java
@@ -0,0 +1,53 @@
+package leetcode.leetcode1116;
+
+import java.util.concurrent.Semaphore;
+import java.util.function.IntConsumer;
+
+public class ZeroEvenOddSync extends ZeroEvenOdd {
+    private final int n;
+    Semaphore semaphoreEven, semaphoreOdd, semaphoreZero;
+
+    public ZeroEvenOddSync(int n) {
+        super(n);
+        this.n = n;
+        semaphoreZero = new Semaphore(1);
+        semaphoreEven = new Semaphore(0);
+        semaphoreOdd = new Semaphore(0);
+    }
+
+    @Override
+    public ZeroEvenOdd getInstance(int n) {
+        return new ZeroEvenOddSync(n);
+    }
+
+    @Override
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            semaphoreZero.acquire();
+            printNumber.accept(0);
+            if ((i & 1) == 0) {
+                semaphoreEven.release();
+            } else {
+                semaphoreOdd.release();
+            }
+        }
+    }
+
+    @Override
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            semaphoreEven.acquire();
+            printNumber.accept(i);
+            semaphoreZero.release();
+        }
+    }
+
+    @Override
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i += 2) {
+            semaphoreOdd.acquire();
+            printNumber.accept(i);
+            semaphoreZero.release();
+        }
+    }
+}
---
+++ src/leetcode/leetcode1116/ZeroEvenOddSyncTest.java
@@ -0,0 +1,47 @@
+package leetcode.leetcode1116;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ZeroEvenOddSyncTest {
+    @Test
+    void testZeroEvenOdd() {
+        IntStream.range(1, 1000).forEach(i -> {
+            StringBuilder sb = new StringBuilder();
+            ZeroEvenOddSync zeroEvenOddSync = new ZeroEvenOddSync(i);
+            CompletableFuture<Void>[] completableFutures = new CompletableFuture[3];
+            completableFutures[0] = CompletableFuture.runAsync(() -> {
+                try {
+                    zeroEvenOddSync.zero(x -> sb.append(' ').append(x));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            completableFutures[1] = CompletableFuture.runAsync(() -> {
+                try {
+                    zeroEvenOddSync.even(x -> sb.append(' ').append(x));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            completableFutures[2] = CompletableFuture.runAsync(() -> {
+                try {
+                    zeroEvenOddSync.odd(x -> sb.append(' ').append(x));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+            CompletableFuture.allOf(completableFutures).join();
+            String[] s = sb.toString().substring(1).split(" ");
+            assertEquals(2 * i, s.length);
+            for (int j = 0; j < i; j++) {
+                assertEquals("0", s[2 * j]);
+                assertEquals(j + 1, Integer.parseInt(s[2 * j + 1]));
+            }
+        });
+    }
+}
