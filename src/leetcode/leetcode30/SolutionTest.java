09f5c9b4b8e0aee8b11a2ddfe4ca199d9160c30a
redsun9 <mokeev.vladimir@gmail.com>
2020-03-19 22:32:19 +0300

    Added leetcode30

---
+++ src/leetcode/leetcode30/SolutionTest.java
@@ -0,0 +1,26 @@
+package leetcode.leetcode30;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+    @Test
+    void findSubstring() {
+        Solution solution = new Solution();
+        assertEquals(
+                new HashSet<>(Arrays.asList(0, 9)),
+                new HashSet<>(solution.findSubstring("barfoothefoobarman", new String[]{"foo", "bar"})
+                )
+        );
+        assertEquals(
+                new HashSet<>(Arrays.asList(6, 9, 12)),
+                new HashSet<>(solution.findSubstring("barfoofoobarthefoobarman", new String[]{"bar", "foo", "the"})
+                )
+        );
+    }
+}
