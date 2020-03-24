bbb07e5db9f13d558e0147b8e9ec791c4921e99f
redsun9 <mokeev.vladimir@gmail.com>
2020-03-24 12:24:31 +0300

    Added kSum to basic

--- src/basic/ArrayTools.java
+++ src/basic/ArrayTools.java
@@ -1,9 +1,6 @@
 package basic;
 
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class ArrayTools {
     private static int sumWithLeastNumberOfElements(int[] a, int k) {
@@ -24,6 +21,42 @@ public class ArrayTools {
         }
         if (s < k) return -1;
         return queue.size();
+    }
+
+    //usage kSum(nums, 0, k, target)
+    private static List<List<Integer>> kSum(int[] nums, int start, int k, int target) {
+        int len = nums.length;
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (k == 2) { //two pointers from left and right
+            int left = start, right = len - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    List<Integer> path = new ArrayList<Integer>();
+                    path.add(nums[left]);
+                    path.add(nums[right]);
+                    res.add(path);
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                } else if (sum < target) { //move left
+                    left++;
+                } else { //move right
+                    right--;
+                }
+            }
+        } else {
+            for (int i = start; i < len - (k - 1); i++) {
+                if (i > start && nums[i] == nums[i - 1]) continue;
+                List<List<Integer>> temp = kSum(nums, i + 1, k - 1, target - nums[i]);
+                for (List<Integer> t : temp) {
+                    t.add(0, nums[i]);
+                }
+                res.addAll(temp);
+            }
+        }
+        return res;
     }
 
     private static void read1dArray(Scanner scanner, int[] a, int n) {
