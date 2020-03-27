6e694d1ad7c2afeaa745f6e13e55c74c9f18436b
redsun9 <mokeev.vladimir@gmail.com>
2020-03-27 15:14:58 +0300

    Added leetcode432 (All O(1) data structure)

---
+++ src/leetcode/leetcode432/AllOneTest.java
@@ -0,0 +1,25 @@
+package leetcode.leetcode432;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AllOneTest {
+
+    @Test
+    void test1() {
+        AllOne allOne = new AllOne();
+        allOne.inc("a");
+        allOne.inc("b");
+        allOne.inc("b");
+        allOne.inc("c");
+        allOne.inc("c");
+        allOne.inc("c");
+        allOne.dec("b");
+        allOne.dec("b");
+        assertEquals("a", allOne.getMinKey());
+        allOne.dec("a");
+        assertEquals("c", allOne.getMaxKey());
+        assertEquals("c", allOne.getMinKey());
+    }
+}
