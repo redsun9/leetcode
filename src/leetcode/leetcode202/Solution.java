6000b9614389112b3a5ec6ffb9f088d38ee50a8b
redsun9 <mokeev.vladimir@gmail.com>
2020-03-30 06:58:07 +0300

    Added leetcode202

---
+++ src/leetcode/leetcode202/Solution.java
@@ -0,0 +1,19 @@
+package leetcode.leetcode202;
+
+import java.util.HashSet;
+
+public class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> cache = new HashSet<>();
+        while (!cache.contains(n) && n != 1) {
+            cache.add(n);
+            int next = 0;
+            while (n != 0) {
+                next += (n % 10) * (n % 10);
+                n /= 10;
+            }
+            n = next;
+        }
+        return n == 1;
+    }
+}
