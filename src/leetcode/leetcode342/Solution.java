35072a7d665ab9a3662606430368687ec8428b50
redsun9 <mokeev.vladimir@gmail.com>
2020-03-30 06:17:05 +0300

    Added leetcode342

---
+++ src/leetcode/leetcode342/Solution.java
@@ -0,0 +1,7 @@
+package leetcode.leetcode342;
+
+public class Solution {
+    public boolean isPowerOfFour(int num) {
+        return num > 0 && (num & (num - 1)) == 0 && (num & 0x55555555) != 0;
+    }
+}
