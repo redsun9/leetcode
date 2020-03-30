a899eff453b91c8467cf3fa87ef7211c5363efcf
redsun9 <mokeev.vladimir@gmail.com>
2020-03-30 06:23:46 +0300

    Added leetcode1310

---
+++ src/leetcode/leetcode1310/Solution.java
@@ -0,0 +1,15 @@
+package leetcode.leetcode1310;
+
+public class Solution {
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] ^= arr[i - 1];
+        }
+        int[] ans = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            if (queries[i][0] != 0) ans[i] = arr[queries[i][0] - 1] ^ arr[queries[i][1]];
+            else ans[i] = arr[queries[i][1]];
+        }
+        return ans;
+    }
+}
