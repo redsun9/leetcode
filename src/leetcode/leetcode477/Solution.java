5e8fb66f924fdb822f4f4b806d3e440ae928f6bc
redsun9 <mokeev.vladimir@gmail.com>
2020-03-30 06:30:38 +0300

    Added leetcode477

---
+++ src/leetcode/leetcode477/Solution.java
@@ -0,0 +1,19 @@
+package leetcode.leetcode477;
+
+public class Solution {
+    public int totalHammingDistance(int[] nums) {
+        int[] bits = new int[32];
+        for (int num : nums) {
+            for (int i = 0; i < 32; i++) {
+                bits[i] += (num & (1 << i)) >> i;
+            }
+        }
+        int ans = 0;
+        int n = nums.length;
+        for (int bit : bits) {
+            ans += bit * (n - bit);
+        }
+        return ans;
+
+    }
+}
