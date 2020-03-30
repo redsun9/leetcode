ba8d8b650a53e6bf7110af79b3fd8059a92a89eb
redsun9 <mokeev.vladimir@gmail.com>
2020-03-30 06:05:39 +0300

    Added leetcode260

---
+++ src/leetcode/leetcode260/Solution.java
@@ -0,0 +1,17 @@
+package leetcode.leetcode260;
+
+public class Solution {
+    public int[] singleNumber(int[] nums) {
+        int t = 0;
+        for (int num : nums) {
+            t ^= num;
+        }
+        int check = 1;
+        while ((t & check) == 0) check <<= 1;
+        int[] ans = new int[2];
+        for (int num : nums) {
+            ans[(num & check) == check ? 1 : 0] ^= num;
+        }
+        return ans;
+    }
+}
