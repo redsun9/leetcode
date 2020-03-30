79b88ae2be985e783b60d690b2c8887d1ecd6e3e
redsun9 <mokeev.vladimir@gmail.com>
2020-03-30 04:15:01 +0300

    Added leetcode710

---
+++ src/leetcode/leetcode710/Solution.java
@@ -0,0 +1,30 @@
+package leetcode.leetcode710;
+
+import java.util.HashMap;
+import java.util.Random;
+
+public class Solution {
+    private HashMap<Integer, Integer> instead = new HashMap<>();
+    private Random random = new Random();
+    private final int total;
+
+    public Solution(int n, int[] blacklist) {
+        total = n - blacklist.length;
+        for (int b : blacklist) {
+            instead.put(b, -1);
+        }
+        for (int b : blacklist) {
+            if (b < total) {
+                while (instead.containsKey(n - 1))
+                    n--;
+                instead.put(b, n - 1);
+                n--;
+            }
+        }
+    }
+
+    public int pick() {
+        int p = random.nextInt(total);
+        return instead.getOrDefault(p, p);
+    }
+}
