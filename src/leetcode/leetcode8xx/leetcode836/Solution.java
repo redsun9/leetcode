6353817efd4f07e1969c3fd8f51494af0ec5f25e
redsun9 <mokeev.vladimir@gmail.com>
2020-04-04 00:40:16 +0300

    Added leetcode836 (intersecting rectangles)

---
+++ src/leetcode/leetcode8xx/leetcode836/Solution.java
@@ -0,0 +1,7 @@
+package leetcode.leetcode8xx.leetcode836;
+
+public class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        return rec1[0] < rec2[2] && rec1[2] > rec2[0] && rec1[1] < rec2[3] && rec1[3] > rec2[1];
+    }
+}
