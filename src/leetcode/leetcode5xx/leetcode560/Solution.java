5b83baa2aa4df8452e60056c5dab7901877a2e6a
redsun9 <mokeev.vladimir@gmail.com>
2020-04-03 22:15:09 +0300

    Added leetcode560 (search of subarrays with target sum)

---
+++ src/leetcode/leetcode5xx/leetcode560/Solution.java
@@ -0,0 +1,21 @@
+package leetcode.leetcode5xx.leetcode560;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int sum = 0, result = 0;
+        Map<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (preSum.containsKey(sum - k)) {
+                result += preSum.get(sum - k);
+            }
+            preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        }
+        return result;
+    }
+}
