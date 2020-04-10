da578a795cb2bdf2eb355364ec8b4f979b95f6dc
redsun9 <mokeev.vladimir@gmail.com>
2020-04-10 07:42:05 +0300

    Added leetcode1290

---
+++ src/leetcode/leetcode12xx/leetcode1290/Solution.java
@@ -0,0 +1,14 @@
+package leetcode.leetcode12xx.leetcode1290;
+
+import leetcode.tools.ListNode;
+
+public class Solution {
+    public int getDecimalValue(ListNode head) {
+        int ans = 0;
+        while (head != null) {
+            ans = ans * 2 + head.val;
+            head = head.next;
+        }
+        return ans;
+    }
+}
