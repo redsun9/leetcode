71edf28f2a3fb2b93f9bd63533cc57424879e3f1
redsun9 <mokeev.vladimir@gmail.com>
2020-04-14 10:46:49 +0300

    Added codeforces round #634 (Div. 3) ProblemС

---
+++ src/codeforces/contest1335/ProblemC.java
@@ -0,0 +1,33 @@
+package codeforces.contest1335;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+public class ProblemC {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int t = scanner.nextInt(); t > 0; t--) {
+            int n = scanner.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = scanner.nextInt();
+            }
+            if (n <= 3) {
+                System.out.println(n / 2);
+                continue;
+            }
+            HashMap<Integer, Integer> countMap = new HashMap<>();
+            int lasrgestSize = 0;
+            for (int ai : a) {
+                int count = countMap.getOrDefault(ai, 0) + 1;
+                lasrgestSize = max(lasrgestSize, count);
+                countMap.put(ai, count);
+            }
+            int groups = countMap.size();
+            System.out.println(max(min(lasrgestSize, groups - 1), min(lasrgestSize - 1, groups)));
+        }
+    }
+}
