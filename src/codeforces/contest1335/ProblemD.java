59d7837838344a1c79cc1296fb969e7241574d1d
redsun9 <mokeev.vladimir@gmail.com>
2020-04-14 16:12:19 +0300

    Added codeforces round #634 (Div. 3) ProblemD

--- src/codeforces/contest1335/ProblemD.java
+++ src/codeforces/contest1335/ProblemD.java
@@ -6,7 +6,9 @@ public class ProblemD {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         for (int t = Integer.parseInt(scanner.nextLine()); t > 0; t--) {
-            System.out.println(scanner.nextLine().replace("2", "1"));
+            for (int i = 0; i < 9; i++) {
+                System.out.println(scanner.nextLine().replace("2", "1"));
+            }
         }
     }
 }
