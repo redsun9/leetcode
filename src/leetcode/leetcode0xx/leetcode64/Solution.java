5d80832cd6aca5bd7c0d162f85aabcf1b3cdd34a
redsun9 <mokeev.vladimir@gmail.com>
2020-04-18 12:58:36 +0300

    Added leetcode64 (minimum path sum)

---
+++ src/leetcode/leetcode0xx/leetcode64/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.leetcode0xx.leetcode64;
+
+public class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 1; i < m; i++) {
+            grid[i][0] += grid[i - 1][0];
+        }
+        for (int i = 1; i < n; i++) {
+            grid[0][i] += grid[0][i - 1];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] += Math.min(grid[i][j - 1], grid[i - 1][j]);
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+}
