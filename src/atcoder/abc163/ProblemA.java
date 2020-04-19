987d5090eeabe8447cc2018844ad01ed0e50eabb
redsun9 <mokeev.vladimir@gmail.com>
2020-04-19 18:34:33 +0300

    Added abc163 ProblemA

---
+++ src/atcoder/abc163/ProblemA.java
@@ -0,0 +1,10 @@
+package atcoder.abc163;
+
+import java.util.Scanner;
+
+public class ProblemA {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(2 * Math.PI * scanner.nextInt());
+    }
+}
