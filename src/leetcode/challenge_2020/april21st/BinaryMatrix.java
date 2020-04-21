77398e487f7f23a82914a76382436cea477a0ea4
redsun9 <mokeev.vladimir@gmail.com>
2020-04-21 10:29:05 +0300

    Added leetcode 30 day challenge problem for April, 21st

---
+++ src/leetcode/challenge_2020/april21st/BinaryMatrix.java
@@ -0,0 +1,9 @@
+package leetcode.challenge_2020.april21st;
+
+import java.util.List;
+
+public interface BinaryMatrix {
+    int get(int x, int y);
+
+    List<Integer> dimensions();
+}
---
+++ src/leetcode/challenge_2020/april21st/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.challenge_2020.april21st;
+
+import java.util.List;
+
+public class Solution {
+    public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
+        List<Integer> dimensions = binaryMatrix.dimensions();
+        int m = dimensions.get(0);
+        int n = dimensions.get(1);
+        int ans = n;
+        while (ans > 0 && m > 0) {
+            m--;
+            int lo = 0;
+            int hi = ans - 1;
+            if (binaryMatrix.get(m, hi) == 0) continue;
+            while (lo < hi) {
+                int mid = lo + (hi - lo) / 2;
+                if (binaryMatrix.get(m, mid) == 0) lo = mid + 1;
+                else hi = mid;
+            }
+            ans = lo;
+        }
+        return ans < n ? ans : -1;
+    }
+}
