58ea7980390c1be825e95b40711baa3ec32e6f61
redsun9 <mokeev.vladimir@gmail.com>
2020-04-28 10:53:24 +0300

    Added leetcode 30day challenge problem for April, 28th

---
+++ src/leetcode/challenge_2020/april28th/FirstUnique.java
@@ -0,0 +1,27 @@
+package leetcode.challenge_2020.april28th;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class FirstUnique {
+    private LinkedHashSet<Integer> unique = new LinkedHashSet<>();
+    private HashSet<Integer> nonUnique = new HashSet<>();
+
+    public FirstUnique(int[] nums) {
+        for (int num : nums) add(num);
+
+    }
+
+    public int showFirstUnique() {
+        if (unique.isEmpty()) return -1;
+        else return unique.iterator().next();
+    }
+
+    public void add(int value) {
+        if (!nonUnique.contains(value))
+            if (unique.contains(value)) {
+                unique.remove(value);
+                nonUnique.add(value);
+            } else unique.add(value);
+    }
+}
