482895d7c8b3fe5ded4e1349c3de51bcdc204efc
redsun9 <mokeev.vladimir@gmail.com>
2020-05-16 13:35:46 +0300

    Added leetcode1329

---
+++ src/leetcode/leetcode13xx/leetcode1329/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.leetcode13xx.leetcode1329;
+
+import java.util.PriorityQueue;
+
+@SuppressWarnings("ConstantConditions")
+public class Solution {
+    public int[][] diagonalSort(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        PriorityQueue<Integer>[] pq = new PriorityQueue[m + n - 1];
+        for (int i = 0; i < m + n - 1; i++) {
+            pq[i] = new PriorityQueue<>();
+        }
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                pq[i - j + n - 1].add(mat[i][j]);
+            }
+        }
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                mat[i][j] = pq[i - j + n - 1].poll();
+        return mat;
+    }
+}
