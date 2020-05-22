e6062c8349f98f2d0b8f02aa9d8241344d506930
redsun9 <mokeev.vladimir@gmail.com>
2020-05-22 10:27:41 +0300

    Added constructors to TreeNode

--- src/leetcode/tools/TreeNode.java
+++ src/leetcode/tools/TreeNode.java
@@ -5,7 +5,16 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
 
+    public TreeNode() {
+    }
+
     public TreeNode(int x) {
         val = x;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
