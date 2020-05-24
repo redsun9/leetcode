14d4e094e32a242e6377b88e3111c54ed1d9e17d
redsun9 <mokeev.vladimir@gmail.com>
2020-05-24 18:01:15 +0300

    Added leetcode237

---
+++ src/leetcode/leetcode2xx/leetcode237/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.leetcode2xx.leetcode237;
+
+import leetcode.tools.ListNode;
+
+public class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
