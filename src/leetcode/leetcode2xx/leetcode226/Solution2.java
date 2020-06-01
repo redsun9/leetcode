0dfe784bfd7becb21a416e131cd650eea3787b54
redsun9 <mokeev.vladimir@gmail.com>
2020-06-01 18:57:20 +0300

    Added leetcode226 (dfs solution)

---
+++ src/leetcode/leetcode2xx/leetcode226/Solution2.java
@@ -0,0 +1,13 @@
+package leetcode.leetcode2xx.leetcode226;
+
+public class Solution2 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
