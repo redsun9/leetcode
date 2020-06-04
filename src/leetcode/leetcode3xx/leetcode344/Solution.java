d083fa327b04d2cce0cc0f339ed2792af00f5736
redsun9 <mokeev.vladimir@gmail.com>
2020-06-04 10:31:03 +0300

    Added leetcode344

---
+++ src/leetcode/leetcode3xx/leetcode344/Solution.java
@@ -0,0 +1,11 @@
+package leetcode.leetcode3xx.leetcode344;
+
+public class Solution {
+    public void reverseString(char[] s) {
+        for (int i = 0, j = s.length - 1; i < j; i++, j--) {
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+        }
+    }
+}
