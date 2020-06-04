1e59f24e9db5070d5d4ff4fd22d09d16418b0f33
redsun9 <mokeev.vladimir@gmail.com>
2020-06-04 10:34:51 +0300

    Updated leetcode1029

--- src/leetcode/leetcode10xx/leetcode1029/Solution.java
+++ src/leetcode/leetcode10xx/leetcode1029/Solution.java
@@ -1,20 +1,14 @@
 package leetcode.leetcode10xx.leetcode1029;
 
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.PriorityQueue;
 
-@SuppressWarnings("ConstantConditions")
 public class Solution {
     public int twoCitySchedCost(int[][] costs) {
-        int n = costs.length;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(x -> costs[x][0] - costs[x][1]));
-        for (int i = 0; i < n; i++) pq.offer(i);
+        Arrays.sort(costs, Comparator.comparingInt(x -> x[0] - x[1]));
         int ans = 0;
-        for (int i = n / 2; i > 0; i--) {
-            ans += costs[pq.poll()][0];
-        }
-        for (int i = n / 2; i > 0; i--) {
-            ans += costs[pq.poll()][1];
+        for (int i = 0, j = costs.length - 1; i < j; i++, j--) {
+            ans += costs[i][0] + costs[j][1];
         }
         return ans;
     }
