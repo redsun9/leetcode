8e2a64adca2ed4c0e30d18458dd9abbbb346eb64
redsun9 <mokeev.vladimir@gmail.com>
2020-06-08 15:39:39 +0300

    Added UnionFind

---
+++ src/basic/UnionFind.java
@@ -0,0 +1,36 @@
+package basic;
+
+public class UnionFind {
+    private final int n;
+    private final int p[], rank[];
+
+    public UnionFind(int n) {
+        this.n = n;
+        rank = new int[n];
+        p = new int[n];
+        for (int i = 0; i < n; i++) {
+            p[i] = i;
+        }
+    }
+
+    int find(int x) {
+        if (x == p[x]) return x;
+        else {
+            p[x] = find(p[x]);
+            return p[x];
+        }
+    }
+
+    void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) return;
+        if (rank[x] < rank[y])
+            p[x] = y;
+        else {
+            p[y] = x;
+            if (rank[x] == rank[y])
+                ++rank[x];
+        }
+    }
+}
