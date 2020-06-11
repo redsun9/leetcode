2b11fac9b7bcd383190dd4844881e09d0c329fc1
redsun9 <mokeev.vladimir@gmail.com>
2020-06-11 07:59:58 +0300

    Optimized 1415

--- src/leetcode/leetcode14xx/leetcode1415/Solution.java
+++ src/leetcode/leetcode14xx/leetcode1415/Solution.java
@@ -2,19 +2,16 @@ package leetcode.leetcode14xx.leetcode1415;
 
 public class Solution {
     public String getHappyString(int n, int k) {
-        int[] a = new int[n];
         k--;
+        char[] c = new char[n];
         for (int i = n - 1; i > 0; i--) {
-            a[i] = k % 2;
+            c[i] = (char) ('a' + k % 2);
             k /= 2;
         }
         if (k > 2) return "";
-        a[0] = k;
-        char[] c = new char[n];
-        c[0] = (char) (a[0] + 'a');
+        c[0] = (char) ('a' + k);
         for (int i = 1; i < n; i++) {
-            if (a[i - 1] <= a[i]) a[i]++;
-            c[i] = (char) (a[i] + 'a');
+            if (c[i - 1] <= c[i]) c[i]++;
         }
         return new String(c);
     }
