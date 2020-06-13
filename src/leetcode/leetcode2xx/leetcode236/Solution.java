db00801f63ec07ffba69eba3cb8c66ae03c1d4c7
redsun9 <mokeev.vladimir@gmail.com>
2020-06-13 07:56:46 +0300

    Added leetcode236

---
+++ src/leetcode/leetcode2xx/leetcode236/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.leetcode2xx.leetcode236;
+
+import leetcode.tools.TreeNode;
+
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        return left == null ? right : right == null ? left : root;
+    }
+}
