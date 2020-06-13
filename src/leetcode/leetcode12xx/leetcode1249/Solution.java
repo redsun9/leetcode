88cb813cfc18ed8d752e91574860eb43ed42d2b8
redsun9 <mokeev.vladimir@gmail.com>
2020-06-13 18:52:52 +0300

    Added leetcode1249

---
+++ src/leetcode/leetcode12xx/leetcode1249/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.leetcode12xx.leetcode1249;
+
+import java.util.Stack;
+
+public class Solution {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < sb.length(); ++i) {
+            if (sb.charAt(i) == '(') st.add(i);
+            if (sb.charAt(i) == ')') {
+                if (!st.empty()) st.pop();
+                else sb.setCharAt(i, '*');
+            }
+        }
+        while (!st.empty())
+            sb.setCharAt(st.pop(), '*');
+        return sb.toString().replace("*", "");
+    }
+}
