f54ee4e192b4e2bf10e5478a26cda43693d9249e
redsun9 <mokeev.vladimir@gmail.com>
2020-06-13 17:11:35 +0300

    Added leetcode636

---
+++ src/leetcode/leetcode6xx/leetcode636/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.leetcode6xx.leetcode636;
+
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        int prevTime = 0;
+        for (String log : logs) {
+            String[] parts = log.split(":");
+            if (!stack.isEmpty()) res[stack.peek()] += Integer.parseInt(parts[2]) - prevTime;
+            prevTime = Integer.parseInt(parts[2]);
+            if (parts[1].equals("start")) stack.push(Integer.parseInt(parts[0]));
+            else {
+                res[stack.pop()]++;
+                prevTime++;
+            }
+        }
+        return res;
+    }
+}
