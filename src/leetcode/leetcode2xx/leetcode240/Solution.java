d4721745b55eac5af2f0b2c0ac617f3f179e571c
redsun9 <mokeev.vladimir@gmail.com>
2020-06-20 10:35:36 +0300

    Added leetcode240

---
+++ src/leetcode/leetcode2xx/leetcode240/Solution.java
@@ -0,0 +1,17 @@
+package leetcode.leetcode2xx.leetcode240;
+
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        if (m == 0) return false;
+        int n = matrix[0].length;
+        if (n == 0) return false;
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target) return true;
+            if (matrix[i][j] > target) j--;
+            else i++;
+        }
+        return false;
+    }
+}
