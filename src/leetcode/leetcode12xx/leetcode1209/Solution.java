98e7fe54fab8ecb9d75053b11908d6f2c56fc7bf
redsun9 <mokeev.vladimir@gmail.com>
2020-06-30 20:31:25 +0300

    Added leetcode1209

---
+++ src/leetcode/leetcode12xx/leetcode1209/Solution.java
@@ -0,0 +1,36 @@
+package leetcode.leetcode12xx.leetcode1209;
+
+import java.util.ArrayDeque;
+
+public class Solution {
+    public String removeDuplicates(String s, int k) {
+        // Напишите ваш код здесь...
+        int n = s.length();
+        if (n < k) return s;
+        ArrayDeque<Character> sc = new ArrayDeque<>();
+        ArrayDeque<Integer> si = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (!sc.isEmpty() && sc.peekLast() == c) {
+                sc.pollLast();
+                int count = si.pollLast() + 1;
+                if (count != k) {
+                    sc.addLast(c);
+                    si.addLast(count);
+                }
+            } else {
+                sc.addLast(c);
+                si.addLast(1);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!sc.isEmpty()) {
+            Character character = sc.pollFirst();
+            Integer count = si.pollFirst();
+            for (int i = 0; i < count; i++) {
+                sb.append(character);
+            }
+        }
+        return sb.toString();
+    }
+}
