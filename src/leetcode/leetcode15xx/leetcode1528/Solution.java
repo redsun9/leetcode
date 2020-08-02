4f119389914e90104df6c7637cb4b12ba563bd6c
redsun9 <mokeev.vladimir@gmail.com>
2020-08-02 22:28:08 +0300

    Added leetcode1528

---
+++ src/leetcode/leetcode15xx/leetcode1528/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.leetcode15xx.leetcode1528;
+
+public class Solution {
+    public String restoreString(String s, int[] indices) {
+        int n = s.length();
+        char[] ans = new char[n];
+        for (int i = 0; i < n; i++) ans[indices[i]] = s.charAt(i);
+        return new String(ans);
+    }
+}
