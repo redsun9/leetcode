0826cbeace26c54d078f0a94161864f43e2a1289
redsun9 <mokeev.vladimir@gmail.com>
2020-08-12 11:11:34 +0300

    Added leetcode191

---
+++ src/leetcode/leetcode1xx/leetcode191/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.leetcode1xx.leetcode191;
+
+public class Solution {
+    public int hammingWeight(int n) {
+        n = n - ((n >>> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >>> 2) & 0x33333333);
+        n = (n + (n >>> 4)) & 0x0f0f0f0f;
+        n = n + (n >>> 8);
+        n = n + (n >>> 16);
+        return n & 0x3f;
+    }
+}
