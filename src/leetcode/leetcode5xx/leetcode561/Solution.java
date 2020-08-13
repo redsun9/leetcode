1bbbce168a8fabf248b91f5325b50db38aaea348
redsun9 <mokeev.vladimir@gmail.com>
2020-08-13 07:48:12 +0300

    Added leetcode561

---
+++ src/leetcode/leetcode5xx/leetcode561/Solution.java
@@ -0,0 +1,14 @@
+package leetcode.leetcode5xx.leetcode561;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            ans += nums[i];
+        }
+        return ans;
+    }
+}
