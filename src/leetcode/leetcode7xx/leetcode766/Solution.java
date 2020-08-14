3e9cb50ade978174ddb5c88919a54e5980e10969
redsun9 <mokeev.vladimir@gmail.com>
2020-08-14 14:11:44 +0300

    Added leetcode766

---
+++ src/leetcode/leetcode7xx/leetcode766/Solution.java
@@ -0,0 +1,14 @@
+package leetcode.leetcode7xx.leetcode766;
+
+public class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] != matrix[i - 1][j - 1]) return false;
+            }
+        }
+        return true;
+    }
+}
