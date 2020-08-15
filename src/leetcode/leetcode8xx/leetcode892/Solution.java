9315c42906f471f62b51e3b105e358816aec440a
redsun9 <mokeev.vladimir@gmail.com>
2020-08-15 08:07:37 +0300

    Added leetcode892

---
+++ src/leetcode/leetcode8xx/leetcode892/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.leetcode8xx.leetcode892;
+
+public class Solution {
+    public int surfaceArea(int[][] grid) {
+        int n = grid.length;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] > 0) ans += grid[i][j] * 4 + 2;
+                if (i > 0) ans -= Math.min(grid[i][j], grid[i - 1][j]) * 2;
+                if (j > 0) ans -= Math.min(grid[i][j], grid[i][j - 1]) * 2;
+            }
+        }
+        return ans;
+    }
+}
