33c1e5222a6208371aa456d7ae7d478664e4b66a
redsun9 <mokeev.vladimir@gmail.com>
2020-09-01 13:56:34 +0300

    Added yandex test contest

---
+++ src/yandex/AB3.java
@@ -0,0 +1,12 @@
+package yandex;
+
+import java.util.Scanner;
+
+public class AB3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        System.out.println(a + b);
+    }
+}
