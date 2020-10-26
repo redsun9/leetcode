dc285e93d4b7f096b74b25a34946b4413caf3805
redsun9 <mokeev.vladimir@gmail.com>
2020-10-27 01:30:08 +0300

    Added leetcode799

---
+++ src/leetcode/leetcode7xx/leetcode799/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.leetcode7xx.leetcode799;
+
+public class Solution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double[] res = new double[query_row + 2];
+        res[0] = poured;
+        for (int row = 1; row <= query_row; row++)
+            for (int i = row; i >= 0; i--)
+                res[i + 1] += res[i] = Math.max(0.0, (res[i] - 1) / 2);
+        return Math.min(res[query_glass], 1.0);
+    }
+}
