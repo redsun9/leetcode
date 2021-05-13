a35820bff5b019169aba5136b98ccd27cbdc5ff0
redsun9 <mokeev.vladimir@gmail.com>
2021-05-13 09:46:13 +0300

    Added leetcode1837

---
+++ src/leetcode/leetcode18xx/leetcode1837/Solution.java
@@ -0,0 +1,12 @@
+package leetcode.leetcode18xx.leetcode1837;
+
+public class Solution {
+    public int sumBase(int n, int k) {
+        int ans = 0;
+        while (n != 0) {
+            ans += n % k;
+            n /= k;
+        }
+        return ans;
+    }
+}
