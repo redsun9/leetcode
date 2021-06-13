3e5a16205f520aaa996d61588360ba330a418434
redsun9 <mokeev.vladimir@gmail.com>
2021-06-13 22:52:38 +0300

    Added ege24

---
+++ src/ege/Problem24.java
@@ -0,0 +1,15 @@
+package ege;
+
+import java.util.Scanner;
+
+public class Problem24 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int ans = 0;
+        String str;
+        while ((str = scanner.nextLine()) != null) {
+            if (str.matches(".*A.R.*")) ans++;
+        }
+        System.out.println(ans);
+    }
+}
