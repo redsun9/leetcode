babaadab0360176a80a311b891c64e25181e168d
redsun9 <mokeev.vladimir@gmail.com>
2021-06-17 09:11:36 +0300

    Added ege2680

---
+++ src/ege/Problem2680.java
@@ -0,0 +1,33 @@
+package ege;
+
+import java.util.Scanner;
+
+public class Problem2680 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int ans = 0;
+        int prevLenA = 0, prevLenB = 0, curLenA, curLenB;
+        int prevA = 0, prevB = 0, currA, currB;
+        for (int i = 0; i < n; i++) {
+            currA = scanner.nextInt();
+            currB = scanner.nextInt();
+            curLenA = 1;
+            curLenB = 1;
+            if (currA == prevB) curLenA = prevLenA + 1;
+            if (currA == prevA) curLenA = Math.max(curLenA, prevLenB + 1);
+
+            if (currB == prevB) curLenB = prevLenA + 1;
+            if (currB == prevA) curLenB = Math.max(curLenB, prevLenB + 1);
+
+            prevLenA = curLenA;
+            prevLenB = curLenB;
+            prevA = currA;
+            prevB = currB;
+
+            ans = Math.max(ans, prevLenA);
+            ans = Math.max(ans, prevLenB);
+        }
+        System.out.println(ans);
+    }
+}
