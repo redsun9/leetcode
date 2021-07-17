414b54f96009761aa94225d754761a2d8286063f
redsun9 <mokeev.vladimir@gmail.com>
2021-07-17 07:16:32 +0300

    Added leetcode150

---
+++ src/leetcode/leetcode1xx/leetcode150/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.leetcode1xx.leetcode150;
+
+import java.util.Stack;
+
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            switch (token) {
+                case "+" -> stack.push(stack.pop() + stack.pop());
+                case "-" -> stack.push(-stack.pop() + stack.pop());
+                case "*" -> stack.push(stack.pop() * stack.pop());
+                case "/" -> {
+                    int b = stack.pop(), a = stack.pop();
+                    stack.push(a / b);
+                }
+                default -> stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+}
