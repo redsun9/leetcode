de25cbc34410eb3fd6784326033dc41599a420ca
redsun9 <mokeev.vladimir@gmail.com>
2021-08-07 18:18:02 +0300

    Added leetcode132

---
+++ src/leetcode/leetcode1xx/leetcode132/Solution.java
@@ -0,0 +1,17 @@
+package leetcode.leetcode1xx.leetcode132;
+
+public class Solution {
+    public int minCut(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) dp[i] = i;
+        for (int i = 0; i < n; i++) {
+            int i1 = i, i2 = i;
+            while (i1 >= 0 && i2 < n && s.charAt(i1) == s.charAt(i2++)) dp[i2] = Math.min(dp[i2], dp[i1--] + 1);
+            i1 = i;
+            i2 = i + 1;
+            while (i1 >= 0 && i2 < n && s.charAt(i1) == s.charAt(i2++)) dp[i2] = Math.min(dp[i2], dp[i1--] + 1);
+        }
+        return dp[n] - 1;
+    }
+}
