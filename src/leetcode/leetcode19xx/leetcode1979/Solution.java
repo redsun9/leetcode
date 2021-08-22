f97672d277461e30ca9f8e085233eaaf6a69a786
redsun9 <mokeev.vladimir@gmail.com>
2021-08-22 07:33:46 +0300

    Added leetcode1979

---
+++ src/leetcode/leetcode19xx/leetcode1979/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.leetcode19xx.leetcode1979;
+
+public class Solution {
+
+    public int findGCD(int[] nums) {
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        return gcd(min, max);
+    }
+
+    //greatest common divisor
+    private static int gcd(int a, int b) {
+        int c;
+        while (b != 0) {
+            c = a % b;
+            a = b;
+            b = c;
+        }
+        return a;
+    }
+}
