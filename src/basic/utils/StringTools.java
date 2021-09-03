7ffe5894c1590ee31346538f75a0fba84a1d75db
redsun9 <mokeev.vladimir@gmail.com>
2021-09-04 00:12:33 +0300

    Added longest common subsequence to StringTools

--- src/basic/utils/StringTools.java
+++ src/basic/utils/StringTools.java
@@ -61,4 +61,31 @@ public class StringTools {
         }
         return ans;
     }
+
+    //longest common subsequence
+    public static String lcs(String x, String y) {
+        int m = x.length(), n = y.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (x.charAt(i) == y.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j + 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+
+        char[] ans = new char[dp[m][n]];
+        int i = 0, j = 0, pos = 0;
+        while (i < m && j < n) {
+            if (x.charAt(i) == y.charAt(j)) {
+                ans[pos++] = x.charAt(i);
+                i++;
+                j++;
+            } else if (dp[i + 1][j] >= dp[i][j + 1]) i++;
+            else j++;
+        }
+        return new String(ans);
+    }
 }
