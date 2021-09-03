eb5eaa38ee6e44b0cf0697679e6ae4e2cf06558f
redsun9 <mokeev.vladimir@gmail.com>
2021-09-03 07:17:46 +0300

    Added second solution for tinkoff xor combination problem

---
+++ src/tinkoff/XorCombination/Solution2.java
@@ -0,0 +1,64 @@
+package tinkoff.XorCombination;
+
+import java.util.Scanner;
+
+@SuppressWarnings("DuplicatedCode")
+public class Solution2 {
+    private final static int p = 998244353;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        System.out.println(solve(n, m));
+    }
+
+    static int solve(int n, int m) {
+        if ((m & 1) != 0) return 0;
+        if (n == 1) return m == 0 ? 1 : 0;
+        if (m == 0) return 1;
+
+        // b[i] = a binomial coefficient = C(n;i) mod p
+        // b[i] - number of ways to choose i elements from n
+        long[] binomial = new long[n + 1];
+        binomial[0] = 1;
+        for (int i = 1, k = n; i <= n; i++, k--) binomial[i] = binomial[i - 1] * reverse(i) % p * k % p;
+
+        int positionOfHighestOneBit = 31 - Integer.numberOfLeadingZeros(m);
+        int[][] cache = new int[positionOfHighestOneBit][m + 1];
+        return dfs(n, m, positionOfHighestOneBit - 1, binomial, cache);
+    }
+
+    private static int dfs(int n, int m, int k, long[] bc, int[][] cache) {
+        if (k == 0) return m <= n ? (int) bc[m] : 0;
+        if (cache[k][m] == 0) {
+            long ans = 0;
+            int weight = 2 << k;
+            for (int i = 0, left = m; i <= n && left >= 0; i += 2, left -= weight) {
+                ans = ans + bc[i] * dfs(n, left, k - 1, bc, cache);
+                if (ans >= p) ans %= p;
+            }
+            cache[k][m] = (int) (ans + 1);
+        }
+        return cache[k][m] - 1;
+    }
+
+
+    // generalized Euclidean algorithm
+    private static int reverse(int a) {
+        int t = 0, newT = 1, r = p, newR = a, q, tmp;
+        while (newR != 0) {
+            q = r / newR;
+            tmp = t - q * newT;
+            t = newT;
+            newT = tmp;
+            tmp = r - q * newR;
+            r = newR;
+            newR = tmp;
+        }
+        if (r > 1) return -1;
+        if (t < 0) t += p;
+        return t;
+    }
+
+}
--- src/tinkoff/XorCombination/SolutionTest.java
+++ src/tinkoff/XorCombination/SolutionTest.java
@@ -2,22 +2,60 @@ package tinkoff.XorCombination;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
+    private static final int MAX_N = 10, MAX_M = 5000;
 
     @Test
     void test1() {
         assertEquals(1, Solution.solve(2, 2));
+        assertEquals(1, Solution2.solve(2, 2));
     }
 
     @Test
     void test2() {
         assertEquals(15, Solution.solve(5, 4));
+        assertEquals(15, Solution2.solve(5, 4));
     }
 
     @Test
     void test3() {
         assertEquals(475, Solution.solve(5, 20));
+        assertEquals(475, Solution2.solve(5, 20));
+    }
+
+    @Test
+    void testCorrectness() {
+        IntStream.rangeClosed(1, MAX_N).parallel().forEach(n ->
+                IntStream.rangeClosed(1, MAX_M).parallel().forEach(m ->
+                        assertEquals(Solution.solve(n, m), Solution2.solve(n, m))
+                )
+        );
+    }
+
+    //31032118600
+    //  514195600
+    @Test
+    void testPerf() {
+        long startTime = System.nanoTime();
+        IntStream.rangeClosed(1, MAX_N).parallel().forEach(n ->
+                IntStream.rangeClosed(1, MAX_M).parallel().forEach(m ->
+                        Solution.solve(n, m)
+                )
+        );
+        long endTime = System.nanoTime();
+        System.out.println(endTime - startTime);
+
+        startTime = System.nanoTime();
+        IntStream.rangeClosed(1, MAX_N).parallel().forEach(n ->
+                IntStream.rangeClosed(1, MAX_M).parallel().forEach(m ->
+                        Solution2.solve(n, m)
+                )
+        );
+        endTime = System.nanoTime();
+        System.out.println(endTime - startTime);
     }
 }
