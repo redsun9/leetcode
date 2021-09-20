d735da4ef08fbe589e43b4e378cdea7b43fcfa90
redsun9 <mokeev.vladimir@gmail.com>
2021-09-20 19:58:43 +0300

    Added solution for (add x to list so median == average)

---
+++ src/ege/Median.java
@@ -0,0 +1,56 @@
+package ege;
+
+import java.util.*;
+
+public class Median {
+    private static double EPS = 1e-6;
+
+    public static List<Double> solve(double[] arr) {
+        int n = arr.length;
+        if (n == 0) throw new IllegalArgumentException("All x are solutions");
+        if (n == 1) return Collections.singletonList(arr[0]);
+
+        Arrays.sort(arr);
+        double sum = 0;
+        for (double num : arr) sum += num;
+
+        List<Double> ans = new ArrayList<>();
+
+        if (n % 2 == 1) {
+            // first case: average = (a[n/2-1]+a[n/2])/2
+            double x1 = (arr[n / 2 - 1] + arr[n / 2]) / 2 * (n + 1) - sum;
+            if (x1 - arr[n / 2 - 1] < EPS) ans.add(x1);
+
+            // second case: average = (a[n/2+1]+a[n/2])/2
+            double x2 = (arr[n / 2 + 1] + arr[n / 2]) / 2 * (n + 1) - sum;
+            if (arr[n / 2 - 1] - x2 < EPS) ans.add(x2);
+
+            //third case: average = (a[n/2]+x)/2
+            double x3 = (2 * sum - (n + 1) * arr[n / 2]) / (n - 1);
+            if (arr[n / 2 - 1] - x3 < EPS && x3 - arr[n / 2 + 1] < EPS) ans.add(x3);
+        } else {
+            //first case: average = a[n/2-1]
+            double x1 = (n + 1) * arr[n / 2 - 1] - sum;
+            if (x1 - arr[n / 2 - 1] < EPS) ans.add(x1);
+
+            //second case: average = a[n/2]
+            double x2 = (n + 1) * arr[n / 2] - sum;
+            if (arr[n / 2] - x2 < EPS) ans.add(x2);
+
+            //third case: average = x
+            double x3 = sum / n;
+            if (arr[n / 2 - 1] - x3 < EPS && x3 - arr[n / 2] < EPS) ans.add(x3);
+        }
+
+        ans.sort(Comparator.naturalOrder());
+        double prev = Double.NEGATIVE_INFINITY, curr;
+        ListIterator<Double> iterator = ans.listIterator();
+        while (iterator.hasNext()) {
+            curr = iterator.next();
+            if (Math.abs(prev - curr) < EPS) iterator.remove();
+            else prev = curr;
+        }
+
+        return ans;
+    }
+}
---
+++ src/ege/MedianTest.java
@@ -0,0 +1,98 @@
+package ege;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MedianTest {
+
+    @Test
+    void test1() {
+        double[] arr = {1, 2, 3, 4};
+        List<Double> expected = List.of(0.0, 2.5, 5.0);
+        List<Double> actual = Median.solve(arr).stream().sorted().toList();
+        assertEquals(expected.size(), actual.size());
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.get(i), actual.get(i), 1e-6);
+        }
+    }
+
+    @Test
+    void test2() {
+        double[] arr = {1, 3, 4};
+        List<Double> expected = List.of(0.0, 2.0, 6.0);
+        List<Double> actual = Median.solve(arr).stream().sorted().toList();
+        assertEquals(expected.size(), actual.size());
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.get(i), actual.get(i), 1e-6);
+        }
+    }
+
+    @Test
+    void test3() {
+        double[] arr = {1, 1, 1, 2};
+        List<Double> expected = List.of(0.0);
+        List<Double> actual = Median.solve(arr).stream().sorted().toList();
+        assertEquals(expected.size(), actual.size());
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.get(i), actual.get(i), 1e-6);
+        }
+    }
+
+    @Test
+    void test4() {
+        double[] arr = {1, 1, 1, 1, 2};
+        List<Double> expected = List.of(0.0);
+        List<Double> actual = Median.solve(arr).stream().sorted().toList();
+        assertEquals(expected.size(), actual.size());
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.get(i), actual.get(i), 1e-6);
+        }
+    }
+
+    @Test
+    void test5() {
+        double[] arr = {1, 2, 4, 8};
+        List<Double> expected = List.of(-5.0, 3.75, 5.0);
+        List<Double> actual = Median.solve(arr).stream().sorted().toList();
+        assertEquals(expected.size(), actual.size());
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.get(i), actual.get(i), 1e-6);
+        }
+    }
+
+    @Test
+    void test6() {
+        double[] arr = {1, 2, 4, 8, 16, 32, 64, 128, 256};
+        List<Double> expected = List.of(-391.0);
+        List<Double> actual = Median.solve(arr).stream().sorted().toList();
+        assertEquals(expected.size(), actual.size());
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.get(i), actual.get(i), 1e-6);
+        }
+    }
+
+    @Test
+    void test7() {
+        double[] arr = {1, 1, 1, 1};
+        List<Double> expected = List.of(1.0);
+        List<Double> actual = Median.solve(arr).stream().sorted().toList();
+        assertEquals(expected.size(), actual.size());
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.get(i), actual.get(i), 1e-6);
+        }
+    }
+
+    @Test
+    void test8() {
+        double[] arr = {1, 1, 1};
+        List<Double> expected = List.of(1.0);
+        List<Double> actual = Median.solve(arr).stream().sorted().toList();
+        assertEquals(expected.size(), actual.size());
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.get(i), actual.get(i), 1e-6);
+        }
+    }
+}
