e0a2b529725eb21f78e65da4361429336eab478b
redsun9 <mokeev.vladimir@gmail.com>
2021-12-04 01:12:53 +0300

    Added advent of code day1 first problem

---
+++ src/advent/day1/first/Solution.java
@@ -0,0 +1,28 @@
+package advent.day1.first;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+@SuppressWarnings({"WrapperTypeMayBePrimitive", "DuplicatedCode"})
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (
+                FileInputStream fis = new FileInputStream("src/advent/day1/first/input.txt");
+                Scanner scanner = new Scanner(fis);
+                FileOutputStream fos = new FileOutputStream("src/advent/day1/first/output.txt");
+                PrintStream printer = new PrintStream(fos)
+        ) {
+            Integer previous = null, curr;
+            int ans = 0;
+            while (scanner.hasNextLine()) {
+                curr = Integer.parseInt(scanner.nextLine().trim());
+                if (previous != null && previous < curr) ans++;
+                previous = curr;
+            }
+            printer.println(ans);
+        }
+    }
+}
