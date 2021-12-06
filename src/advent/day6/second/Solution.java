f039ee33f8fe1895d570a98a34b68d31bf79dccb
redsun9 <mokeev.vladimir@gmail.com>
2021-12-06 09:16:05 +0300

    Added advent of code day6 second problem

---
+++ src/advent/day6/second/Solution.java
@@ -0,0 +1,38 @@
+package advent.day6.second;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+@SuppressWarnings({"DuplicatedCode", "SuspiciousSystemArraycopy"})
+public class Solution {
+    private static final int numberOfDays = 256;
+
+    public static void main(String[] args) throws IOException {
+        try (
+                FileInputStream fis = new FileInputStream("src/advent/day6/second/input.txt");
+                Scanner scanner = new Scanner(fis);
+                FileOutputStream fos = new FileOutputStream("src/advent/day6/second/output.txt");
+                PrintStream printer = new PrintStream(fos)
+        ) {
+            long[] prev = new long[9];
+            String s = scanner.nextLine().trim();
+            int length = s.length();
+            for (int i = 0; i < length; i += 2) prev[s.charAt(i) - '0']++;
+
+
+            for (int i = 0; i < numberOfDays; i++) {
+                long newFishes = prev[0];
+                System.arraycopy(prev, 1, prev, 0, 8);
+                prev[6] += newFishes;
+                prev[8] = newFishes;
+            }
+
+            long ans = 0;
+            for (int i = 0; i < 9; i++) ans += prev[i];
+            printer.println(ans);
+        }
+    }
+}
