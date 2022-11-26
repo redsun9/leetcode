d2dc86d66dcf1ecba92bc67514f25925483f8685
redsun9 <mokeev.vladimir@gmail.com>
2022-11-27 00:28:45 +0600

    Added leetcode2468

---
+++ src/leetcode/leetcode24xx/leetcode2469/Solution.java
@@ -0,0 +1,7 @@
+package leetcode.leetcode24xx.leetcode2469;
+
+public class Solution {
+    public double[] convertTemperature(double celsius) {
+        return new double[]{celsius + 273.15, celsius * 1.80 + 32.00};
+    }
+}
