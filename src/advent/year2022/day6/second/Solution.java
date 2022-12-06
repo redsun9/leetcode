a870fd09d20cbbda898a6627ff8c42541d67d556
redsun9 <mokeev.vladimir@gmail.com>
2022-12-07 05:01:38 +0600

    Added advent of code 2022 day 6 second

---
+++ src/advent/year2022/day6/second/Solution.java
@@ -0,0 +1,33 @@
+package advent.year2022.day6.second;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+@SuppressWarnings({"WrapperTypeMayBePrimitive", "DuplicatedCode"})
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (
+                FileInputStream fis = new FileInputStream("src/advent/year2022/day6/second/input.txt");
+                Scanner scanner = new Scanner(fis);
+                FileOutputStream fos = new FileOutputStream("src/advent/year2022/day6/second/output.txt");
+                PrintStream printer = new PrintStream(fos)
+        ) {
+            String s = scanner.nextLine();
+            int[] cnt = new int[26];
+            int total = 0, n = s.length();
+            for (int r = 0, l = 0; r < n; r++) {
+                if (cnt[s.charAt(r) - 'a']++ == 0) total++;
+                if (r >= 14) {
+                    if (--cnt[s.charAt(l++) - 'a'] == 0) total--;
+                }
+                if (total == 14) {
+                    printer.println(r + 1);
+                    return;
+                }
+            }
+        }
+    }
+}
