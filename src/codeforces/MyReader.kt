296e7a78506de0e8e15622f3f1e9941b69834ffc
redsun9 <mokeev.vladimir@gmail.com>
2023-01-03 23:09:06 +0300

    Added kotlin reader for contests

---
+++ src/codeforces/MyReader.kt
@@ -0,0 +1,33 @@
+package codeforces
+
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.util.*
+
+class MyReader(inputStream: InputStream) {
+    private val reader = BufferedReader(InputStreamReader(inputStream))
+    private var tokenizer: StringTokenizer? = null
+
+    fun next(): String {
+        while (true) {
+            tokenizer.let {
+                if (it == null || !it.hasMoreTokens()) {
+                    tokenizer = StringTokenizer(reader.readLine())
+                } else {
+                    return it.nextToken()
+                }
+            }
+        }
+    }
+}
+
+val reader = MyReader(System.`in`)
+fun next() = reader.next()
+fun nextInt() = next().toInt()
+fun nextLong() = next().toLong()
+fun nextDouble() = next().toDouble()
+fun nextStrings(n: Int) = Array(n) { next() }
+fun nextInts(n: Int) = IntArray(n) { nextInt() }
+fun nextLongs(n: Int) = LongArray(n) { nextLong() }
+fun nextDoubles(n: Int) = DoubleArray(n) { nextDouble() }
