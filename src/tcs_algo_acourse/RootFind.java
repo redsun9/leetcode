8cb5f3c34102047bc0b3c719aebab77559d7380e
redsun9 <mokeev.vladimir@gmail.com>
2023-02-18 01:16:22 +0600

    Added root_find to tcs_algo_course

---
+++ src/tcs_algo_acourse/RootFind.java
@@ -0,0 +1,23 @@
+package tcs_algo_acourse;
+
+import java.util.Scanner;
+
+public class RootFind {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double c = Double.parseDouble(scanner.nextLine().trim());
+        System.out.println(findRoot(c));
+    }
+
+
+    private static double findRoot(double c) {
+        double lo = 0, hi = Math.sqrt(c);
+        while (Math.abs(lo - hi) >= 1e-9) {
+            double mid = (lo + hi) / 2.0;
+            double x = mid * mid + Math.sqrt(mid + 1.0);
+            if (x < c) lo = mid;
+            else hi = mid;
+        }
+        return lo;
+    }
+}
