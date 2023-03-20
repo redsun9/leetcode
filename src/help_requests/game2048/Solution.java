ccacbd4b31651317c414d3ea99e952014b06025f
redsun9 <mokeev.vladimir@gmail.com>
2023-03-20 10:20:11 +0300

    Added game2048 to help_requests

--- src/help_requests/game2048/Solution.java
+++ src/help_requests/game2048/Solution.java
@@ -15,6 +15,17 @@ public class Solution {
         }
     }
 
+    public static void processLeftSwipeWithoutCascade(int[] row) {
+        int n = row.length;
+        for (int l = -1, r = 0; r < n; r++) {
+            if (row[r] == 0) continue;
+            int num = row[r];
+            row[r] = 0;
+            if (l >= 0 && row[l] == num) row[l++] = num * 2;
+            else row[++l] = num;
+        }
+    }
+
     public static void processRightSwipe(int[] row) {
         int n = row.length;
         for (int r = n, l = n - 1; l >= 0; l--) {
@@ -26,6 +37,17 @@ public class Solution {
                 num <<= 1;
             }
             row[--r] = num;
+        }
+    }
+
+    public static void processRightSwipeWithoutCascade(int[] row) {
+        int n = row.length;
+        for (int r = n, l = n - 1; l >= 0; l--) {
+            if (row[l] == 0) continue;
+            int num = row[l];
+            row[l] = 0;
+            if (r < n && row[r] == num) row[r--] = num * 2;
+            else row[--r] = num;
         }
     }
 }
