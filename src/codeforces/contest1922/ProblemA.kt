9c75daef7deffc9cdc8cce9486aa7dce7741bb97
redsun9 <mokeev.vladimir@gmail.com>
2024-01-26 00:10:13 +0300

    Added codeforces contest1922 problemA

---
+++ src/codeforces/contest1922/ProblemA.kt
@@ -0,0 +1,46 @@
+@file:Suppress("DuplicatedCode")
+
+package codeforces.contest1922
+
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.util.*
+
+fun main(args: Array<String>) {
+    val tests = nextInt()
+    for (t in 0 until tests) {
+        val n = nextInt()
+        println(if (solve(next(), next(), next())) "YES" else "NO")
+    }
+}
+
+fun solve(a: String, b: String, c: String) = a.indices.any { i -> a[i] != c[i] && b[i] != c[i] }
+
+
+private class MyReader2(inputStream: InputStream) {
+    private val reader = BufferedReader(InputStreamReader(inputStream))
+    private var tokenizer: StringTokenizer? = null
+
+    fun next(): String {
+        while (true) {
+            tokenizer.let {
+                if (it == null || !it.hasMoreTokens()) {
+                    tokenizer = StringTokenizer(reader.readLine())
+                } else {
+                    return it.nextToken()
+                }
+            }
+        }
+    }
+}
+
+private val reader = MyReader2(System.`in`)
+private fun next() = reader.next()
+private fun nextInt() = next().toInt()
+private fun nextLong() = next().toLong()
+private fun nextDouble() = next().toDouble()
+private fun nextStrings(n: Int) = Array(n) { next() }
+private fun nextInts(n: Int) = IntArray(n) { nextInt() }
+private fun nextLongs(n: Int) = LongArray(n) { nextLong() }
+private fun nextDoubles(n: Int) = DoubleArray(n) { nextDouble() }
